3fc16cbb94023f8139b29a300d6a8f92691f6a45
Veranika Ihoshava <ihoshava.veranika@gmail.com>
2013-07-03 21:34:52 +0300

    8 строк так 8 строк (views.py)

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -4,13 +4,10 @@ from django.shortcuts import render
 import datetime
 
 def apply_form(request):
+    form = ApplicationForm()
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid() and request.POST['checked']:
             form.save()
             return HttpResponse('Saved to database')
-        else:
-            return render(request, 'shop/application_form.html', {'form': form})
-    else:
-        form = ApplicationForm()
-        return render(request, 'shop/application_form.html', {'form': form})
+    return render(request, 'shop/application_form.html', {'form': form})
