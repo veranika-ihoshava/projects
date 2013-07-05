48f3790630b0fea333d622d6d14d231bd45d465b
Veranika Ihoshava <ihoshava.veranika@gmail.com>
2013-07-06 01:06:39 +0300

    Реализовать индексную страницу магазина

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
 from forms import ApplicationForm
+from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
+
 
 def apply_form(request):
-    form = ApplicationForm()
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
-        if form.is_valid() and request.POST['checked']:
+        if form.is_valid():
             form.save()
             return HttpResponse('Saved to database')
+    else:
+        form = ApplicationForm()
     return render(request, 'shop/application_form.html', {'form': form})
