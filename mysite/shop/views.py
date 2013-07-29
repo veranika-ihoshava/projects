7c52c67205e195ebcb44fcc82e2df35f6c0ea272
Veranika Ihoshava <ihoshava.veranika@gmail.com>
2013-07-29 14:57:00 +0300

    Добавить страницу списка товаров в сессии, пока без количества товаров

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -15,12 +15,33 @@ def apply_form(request):
         form = ApplicationForm()
     return render(request, 'shop/application_form.html', {'form': form})
 
+
 @require_GET
-def add_to_basket(request, product_id = 'product_id'):
-    if Product.objects.filter(id=product_id).exists():
+def add_to_basket(request, product_id='product_id'):
+    if Product.objects.filter(id=product_id).exists(): #what for this???
         if 'product_id' in request.session:
             request.session.get('product_id').append(product_id)
             request.session.modified = True
         else:
             request.session['product_id'] = [product_id]
     return HttpResponseRedirect('/products/')
+
+@require_GET
+def delete_from_basket(request, product_id='product_id'):
+    if Product.objects.filter(id=product_id).exists():
+        if 'product_id' in request.session:
+            request.session.get('product_id').remove(product_id)
+            request.session.modified = True
+    return HttpResponseRedirect('/basket/')
+
+
+def session_products(request, product_id='product_id'):
+    if product_id in request.session:
+        product_id_list = request.session.get(product_id)
+        Product.objects.filter(id__in=product_id_list)
+        return render(request, 'shop/product_list_basket.html',
+                  {'product_list': Product.objects.filter(id__in=product_id_list)})
+    else:
+        return render(request, 'shop/product_list_basket.html',
+                      {'product_list': []})
+
